4343a733f9396a748e64f43638c4f7f8f1d80a44
MatteoLucerni <lucernimatteo.off@gmail.com>
2023-06-01 18:16:38 +0200

    added  static preview

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ console.log('JS OK');
 
 const imagesNames = ['01', '02', '03', '04', '05'];
 const gallery = document.getElementById('imgPlacer');
+const preview = document.getElementById('preview');
 const previous = document.getElementById('previous');
 const next = document.getElementById('next');
 let imgIndex = 0;
@@ -9,9 +10,10 @@ let imgIndex = 0;
 for (let i = 0; i < imagesNames.length; i++){
     gallery.innerHTML += `<img src="img/${imagesNames[i]}.webp"></img>`;
     console.log('added image');
+    preview.innerHTML += `<div class="preview-cont"><img src="img/${imagesNames[i]}.webp"></img></div>`;
 };
 
-const images = document.querySelectorAll('img');
+const images = document.querySelectorAll('#imgPlacer img');
 console.log(images)
 
 images[0].classList.add('active')
