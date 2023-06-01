2a6ecd0d60c320418714fa5e33bb72898cdf7efc
MatteoLucerni <lucernimatteo.off@gmail.com>
2023-06-01 17:36:16 +0200

    added basic carousel

--- js/script.js
+++ js/script.js
@@ -1,1 +1,34 @@
 console.log('JS OK');
+
+const imagesNames = ['01', '02', '03', '04', '05'];
+const gallery = document.getElementById('imgPlacer');
+const previous = document.getElementById('previous');
+const next = document.getElementById('next');
+let imgIndex = 0;
+
+for (let i = 0; i < imagesNames.length; i++){
+    gallery.innerHTML += `<img src="img/${imagesNames[i]}.webp"></img>`;
+    console.log('added image');
+};
+
+const images = document.querySelectorAll('img');
+console.log(images)
+
+images[0].classList.add('active')
+
+next.addEventListener('click', function(){
+    images[imgIndex].classList.remove('active')
+    imgIndex++;
+    console.log(imgIndex)
+    images[imgIndex].classList.add('active')
+});
+
+previous.addEventListener('click', function(){
+    images[imgIndex].classList.remove('active')
+    imgIndex--;
+    console.log(imgIndex)
+    images[imgIndex].classList.add('active')
+});
+
+
+
